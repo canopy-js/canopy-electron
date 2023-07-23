d01b7c687a7db1336e7c1a7f5ca86d42048b7f05
canopy-js-user <47240210+canopy-js-user@users.noreply.github.com>
2023-07-23 15:03:26 -0400

    Remove illegal windows filename characters

--- set-name.js
+++ set-name.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let indexFileText = fs.readFileSync('build/index.html').toString();
 let defaultTopicName = indexFileText.match(/data-default-topic-mixed-case="(.*)"/)[1];
+defaultTopicName = defaultTopicName.replace(/[!><:"|?*/\\]/g, '')
 let packageJson = fs.readFileSync('package.json').toString();
 packageJson = packageJson.replace(/Reader/g, defaultTopicName);
 packageJson = packageJson.replace(/canopy-electron/g, defaultTopicName);
