9a2c8344d6c300caa63690e1409aa8bbcd3893a0
canopy-js-user <47240210+canopy-js-user@users.noreply.github.com>
2023-07-23 14:47:06 -0400

    Use mixed-case name now included on default index.html file
    Previously we used the display form of the default topic
    when naming Electron binaries, but it might have style
    characters so now Canopy includes a mixed case form on
    the Canopy element and we look for that during the Electron
    build process.

--- set-name.js
+++ set-name.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 let indexFileText = fs.readFileSync('build/index.html').toString();
-let defaultTopicName = indexFileText.match(/data-default-topic="(.*)"/)[1];
+let defaultTopicName = indexFileText.match(/data-default-topic-mixed-case="(.*)"/)[1];
 let packageJson = fs.readFileSync('package.json').toString();
 packageJson = packageJson.replace(/Reader/g, defaultTopicName);
 packageJson = packageJson.replace(/canopy-electron/g, defaultTopicName);
