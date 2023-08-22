37cebad024e3cc00de3aec4c528b99c36437a0bc
canopy-js-user <47240210+canopy-js-user@users.noreply.github.com>
2023-08-22 12:49:46 -0400

    Replace characters invalid on windows
    There are certain characters in filenames that are invalid on
    windows but valid elsewhere, so we will selectively remove
    them for windows only.

--- set-name.js
+++ set-name.js
@@ -1,7 +1,8 @@
 let fs = require('fs');
 let indexFileText = fs.readFileSync('build/index.html').toString();
 let defaultTopicName = indexFileText.match(/data-default-topic-mixed-case="(.*)"/)[1];
-defaultTopicName = defaultTopicName.replace(/[!><:"|?*/\\]/g, '')
+defaultTopicName = defaultTopicName.replace(/[><:|?*/\\]/g, '')
+if (process.platform === "win32") defaultTopicName = defaultTopicName.replace(/['"“”‘’]/g, '');
 let packageJson = fs.readFileSync('package.json').toString();
 packageJson = packageJson.replace(/Reader/g, defaultTopicName);
 packageJson = packageJson.replace(/canopy-electron/g, defaultTopicName);
